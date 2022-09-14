2da3ded25bc774840e74277dc66e6043103b6cea
JefferyTang23 <jtang30@stuy.edu>
2022-09-14 14:05:03 -0400

    oink2

--- cbwu1.py
+++ cbwu1.py
@@ -19,3 +19,16 @@ def parrot_trouble(talking, hour):
 
 def makes10(a, b):
   return a == 10 or b == 10 or (a + b) == 10
+
+def near_hundred(n):
+  return abs(100-n) <= 10 or abs(200-n) <= 10
+
+def pos_neg(a, b, negative):
+  if negative:
+    return a < 0 and b < 0
+  return (a > 0 and b < 0) or (a < 0 and b > 0)
+
+def not_string(str):
+  if len(str) > 2 and str[:3] == 'not':
+    return str
+  return 'not ' + str
