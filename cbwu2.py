a94a3cf4dba33fa137f8a0988547a94456c07d25
April Li <april.li0427@gmail.com>
2022-09-15 14:08:08 -0400

    oinkkk2

--- cbwu2.py
+++ cbwu2.py
@@ -30,3 +30,18 @@ def string_splosion(str):
     string += str[:index]
     index += 1
   return string
+
+def last2(str): #incomplete
+  count = 0
+  index = 0
+  while index < len(str):
+    index2 = index+1
+    string = str[index:index+2]
+    while index2 < len(str):
+      string2 = str[index2:index2+2]
+      if string == string2:
+        count += 1
+      index2 += 1
+    index += 1
+  return count
+      
